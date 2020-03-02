9dac043626b492eb7c7f5b57a11606ca88182346
danish <cu.16bcs1331@gmail.com>
2020-03-02 12:28:48 +0530

    initial commit

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,10 +1,10 @@
 package hello;
 
-public class Helloworld {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("helo");
+
 	}
 
 }
